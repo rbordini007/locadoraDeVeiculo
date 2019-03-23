10763a070f95c71063ad48ceacb039ffdfee191a
rbordini007 <ricardo.sampaio@edu.unipar.br>
2019-03-23 14:56:32 -0300

    Inicil do programa Locadora de Veiculo

---
+++ src/program/ProgramPrincipal.java
@@ -0,0 +1,54 @@
+package program;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.service.BrazilTaxService;
+import model.service.RentalService;
+
+public class ProgramPrincipal {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+		
+		System.out.println("Enter rental data: ");
+		System.out.print("Car Model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Pickup (dd/MM/yyyy HH:ss): " );
+		Date start = sdf.parse(sc.nextLine());
+		System.out.print("Return (dd/MM/yyyy HH:ss): ");
+		Date finish = sdf.parse(sc.nextLine());
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.println("Enter price per hour: ");
+		double priceHour = sc.nextDouble();
+		System.out.println("Enter price per day: ");
+		double priceDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(priceHour, priceDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("INVOICE:");
+		
+		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
